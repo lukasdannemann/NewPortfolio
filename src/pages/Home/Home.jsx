b762b2dd0bd4bba1410e49788fdaeef182fe748e
lukasdannemann <lukasdannemann@outlook.com>
2026-03-20 17:05:49 +0100

    add small headers on homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,16 @@
 import Hero from '../../Components/Hero/Hero'
-import ProjectCard from '../../Components/ProjectCard/ProjectCard'
 import ProjectsDiv from '../../Components/ProjectsDiv/ProjectsDiv'
+import './Home.css'
 
 export default function Home() {
 
 
     return (
         <div className='page-content'>
+            <p className='small-header'><span className="about-label">&gt; intro</span></p>
             <Hero />
-            <div>
-            <h3><span className="about-label">&gt; projects</span></h3>
+            <div className='projects-home'>
+            <p className='small-header'><span className="about-label">&gt; projects</span></p>
             <ProjectsDiv />
             </div>
         </div>
